cff61f7ec7a0995c3dce6aed31253773f6e20c34
marlon florez <marlonfg1234@gmail.com>
2023-05-19 17:54:08 -0500

    Se agrega el header y nav con sus estilos y links posible a cambios

--- view/js/script.js
+++ view/js/script.js
@@ -10,3 +10,15 @@ dropdowns.forEach(dropdown => {
     dropdown.classList.toggle('active');
   });
 });
+
+
+const menuIcon = document.querySelector('.menu-icon');
+const menuItems = document.querySelector('.menu');
+
+function toggleMenu() {
+  menuIcon.classList.toggle('open');
+  menuItems.classList.toggle('open');
+}
+
+menuIcon.addEventListener('click', toggleMenu);
+
